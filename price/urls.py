49d578d168824a745a2267997d5b7560a42bb7c9
mrneerajnegi <mr.neerajnegi@gmail.com>
2022-03-24 12:54:25 +0530

    added bike module

--- price/urls.py
+++ price/urls.py
@@ -4,6 +4,8 @@ from . import views
 app_name="price"
 urlpatterns=[
     path('',views.Home.as_view(),name="index"),
-    path('item/<item>', views.InputPage.as_view(), name="item"),
+    path('car/', views.Car.as_view(),name="car"),
+    path('bike/', views.Bike.as_view(),name="bike"),
+    path('house/', views.House.as_view(),name="house"),
 
 ]
--- price/views.py
+++ price/views.py
@@ -3,57 +3,109 @@ from django.views.generic import TemplateView
 import pickle
 import pandas as pd
 # Create your views here.
-carModel = pickle.load(open("/Users/user/Negi/Django/Pridiction_proj/price/car.pkl", "rb"))
 
+bikeModel = pickle.load(open("price/bike.pkl", "rb"))
+carModel= pickle.load(open("price/car.pkl", "rb"))
 
 class Home(TemplateView):
     template_name = "price/index.html"
-    items = ["Car", "Bike", "House"]
+    items = ["Bike", "Car", "House"]
     def post(self,request):
         selectedItem=request.POST["item"]
         if selectedItem=="Please select type...":
             return render(request, "price/index.html",{"error":"Please select type","items": self.items})
+        elif selectedItem==self.items[0]:
+            return redirect("bike/")
+        elif selectedItem == self.items[1]:
+            return redirect("car/")
         else:
-            print(selectedItem)
-            return redirect("item/"+selectedItem)
+            return redirect("house/")
     def get(self,request):
         return render(request, "price/index.html", {"items": self.items})
 
-class InputPage(TemplateView):
-    template_name = "price/input_page.html"
+
+
+class Bike(TemplateView):
+    template_name = "price/bike.html"
+    owner=["First Owner","Second Owner","Third Owner","Fourth Owner Or More"]
+    brand=['Bajaj', 'Hero', 'Royal Enfield', 'Yamaha', 'Honda', 'Suzuki', 'TVS',
+       'KTM', 'Harley-Davidson', 'Kawasaki', 'Hyosung', 'Benelli', 'Mahindra',
+       'Triumph', 'Ducati', 'BMW', 'Jawa', 'MV', 'Indian', 'Ideal', 'Rajdoot',
+       'LML', 'Yezdi']
+    age=[1,2,3,4,5,6,7,8,9,10,11,12]
+
+    def get(self, request, *args, **kwargs):
+        return render(request,"price/bike.html",
+                      {
+                       "owner":self.owner,
+                       "brand":self.brand,
+                       "age":self.age
+                       })
+
+    def post(self,request):
+        print(request.POST)
+        if request.POST["owner"]=="Select Owner" or   request.POST["brand"]=="Select Brand Name" or request.POST["age"]=="Select Age" or not request.POST["kms_driven"] or not request.POST["power"]:
+            return render(request, "price/bike.html",
+                          {"error": "Please fill all fields",
+                           "owner": self.owner,
+                           "brand": self.brand,
+                           "age": self.age})
+        else:
+            data=pd.DataFrame([{"kms_driven": int(request.POST["kms_driven"]),"owner": request.POST["owner"],"age":int(request.POST["age"]),  "power":int(request.POST["power"]), "brand": request.POST["brand"],}])
+            print(data)
+            prediction=bikeModel.predict(data)
+            print(prediction)
+            successMsg="Market price will be around "+str(int(prediction))
+            return render(request, "price/bike.html",
+                          {
+                           "owner":self.owner,
+                            "brand":self.brand,
+                             "age":self.age,
+                             "successMsg":successMsg
+                           })
+
+
+
+
+class Car(TemplateView):
+    template_name = "price/car.html"
     fuel_type=["Diesel","LPG","Petrol"]
     vehiclename=['Audi', 'BMW', 'Chevrolet', 'Datsun', 'Fiat', 'Force', 'Ford',
      'Hindustan', 'Honda', 'Hyundai', 'Jaguar', 'Jeep', 'Land',
      'Mahindra', 'Maruti', 'Mercedes', 'Mini', 'Mitsubishi', 'Nissan',
      'Renault', 'Skoda', 'Tata', 'Toyota', 'Volkswagen', 'Volvo']
     model=[2007,2008,2009,2010,2011,2012,2013,2014,2015,2016,2017,2018,2019,2020,2021]
-    def get(self, request, item, *args, **kwargs):
-        return render(request,"price/input_page.html",
-                      {"item":item,
+    def get(self, request, *args, **kwargs):
+        return render(request,"price/car.html",
+                      {
                        "vehiclename":self.vehiclename,
                        "fuel_type":self.fuel_type,
                        "models":self.model
                        })
 
-    def post(self,request,item):
+    def post(self,request):
         print(request.POST)
         if request.POST["name"]=="Select Vehicle Name" or   request.POST["fuel"]=="Select Fuel Type" or request.POST["model"]=="Select Model" or not request.POST["kms_driven"] :
-            return render(request, "price/input_page.html",
+            return render(request, "price/car.html",
                           {"error": "Please fill all fields",
-                            "item": item,
                            "vehiclename": self.vehiclename,
                            "fuel_type": self.fuel_type,
                            "models": self.model})
         else:
-            data=pd.DataFrame.from_dict([{"company": request.POST["name"], "year": int(request.POST["model"]), "kms_driven": int(request.POST["kms_driven"]), "fuel_type":request.POST["fuel"]}])
+            data=pd.DataFrame([{"company": request.POST["name"], "year": int(request.POST["model"]), "kms_driven": int(request.POST["kms_driven"]), "fuel_type":request.POST["fuel"]}])
             print(data)
             prediction=carModel.predict(data)
-            print(prediction[0][0])
-            successMsg="Market price will be arround "+str(int(prediction[0][0]))
-            return render(request, "price/input_page.html",
-                          {"item": item,
+            print(prediction)
+            successMsg="Market price will be around "+str(int(prediction))
+            return render(request, "price/car.html",
+                          {
                            "vehiclename": self.vehiclename,
                            "fuel_type": self.fuel_type,
                            "models": self.model,
                            "successMsg":successMsg
                            })
+
+class House(TemplateView):
+    template_name = "price/house.html"
+
+
